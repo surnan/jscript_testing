6929df92b57a4363f640791d8bd04053ec07bdd8
surnan <surnan@users.noreply.github.com>
2023-10-01 18:06:53 -0400

    testing javascript buttons

---
+++ scripts/jscripts.js
@@ -0,0 +1,12 @@
+
+function amazonButtonUI(){
+  console.log('hello world!')
+}
+
+function uberScheduleForLaterButtonUI(){
+  console.log('Schedule Later!')
+}
+
+function uberRequestNowButtonUI(){
+  console.log('Request Now!')
+}
