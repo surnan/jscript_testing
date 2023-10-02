f58a837c7ad4fdad1f177598a6de8705d1b0a756
surnan <surnan@users.noreply.github.com>
2023-10-01 21:55:39 -0400

    ADD: Add to Cart Button working

--- scripts/jscripts.js
+++ scripts/jscripts.js
@@ -1,12 +1,57 @@
 
-function amazonButtonUI(){
-  console.log('hello world!')
+function amazonButtonUI() {
+  const buttonElement = document.querySelector('.amazon-cart-button');
+
+  if (buttonElement.innerText === 'Add to Cart'){
+    buttonElement.innerHTML = 'Remove from cart';
+    buttonElement.classList.remove('js-subscribe-button');
+  } else {
+    buttonElement.innerHTML = 'Add to Cart';
+    buttonElement.classList.add('js-subscribe-button');
+  }
 }
 
-function uberScheduleForLaterButtonUI(){
-  console.log('Schedule Later!')
+function uberButtonUI(temp) {
+  let temp2 = `.${temp}`;
+  let chosenButton = document.querySelector(temp2);
+  let nowButton = document.querySelector('.uber-button-now');
+  let laterButton = document.querySelector('.uber-button-later');
+
+  console.log('cc', typeof (laterButton.style.backgroundColor));
+
+  if (nowButton.style.backgroundColor === '.black') {
+    console.log('triggered by black');
+  } else {
+    console.log('triggered by gray');
+  }
 }
 
-function uberRequestNowButtonUI(){
-  console.log('Request Now!')
-}
+
+
+
+/*
+
+  // console.log('temp =', temp);
+
+
+  // if (temp.style.backgroundColor === 'black') {
+  //   console.log('black found');
+  // } else {
+  //   console.log('gray found');
+  // }
+
+  // nowButton.style.backgroundColor = 'orange';
+  // laterButton.style.backgroundColor = 'blue';
+  // chosenButton.style.backgroundColor = 'purple';
+
+  // console.log('currently = ', chosenButton.style.backgroundColor);
+  // console.log('currently = ', nowButton.style.backgroundColor);
+  // console.log('currently = ', laterButton.style.backgroundColor);
+
+
+
+  // console.log(chosenButton);
+  // console.log('nowButton', nowButton);
+  // console.log('laterButton', laterButton);
+  // console.log('=============');
+*/
